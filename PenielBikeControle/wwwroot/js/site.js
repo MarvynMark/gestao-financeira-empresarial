2ac58b9cf458f477c6e8d476c1a990f1cbe622a7
Marvyn Mark <marvyn.mark@gmail.com>
2023-08-25 11:24:53 -0300

    - Ajuste no js e css do site.

--- PenielBikeControle/wwwroot/js/site.js
+++ PenielBikeControle/wwwroot/js/site.js
@@ -1,29 +1,75 @@
-﻿const tituloDaPagina = '#titulo-da-pagina';
-const tituloDaPaginaLayout = "#titulo-da-pagina-layout";
+﻿(function () {
+    const tituloDaPagina = '#titulo-da-pagina';
+    const tituloDaPaginaLayout = "titulo-da-pagina-layout";
+    const btnFecharModal = '.btn-fechar-modal';
 
-IniciaElementos();
+    IniciaElementos();
 
-$(document).ready(function () {
-    $('.table').DataTable();
 
-    $(".select").select2({
-        placeholder: 'Selecione...',
-        theme: "classic",
-        language: {
-            noResults: function () {
-                return "Informação não encontrada!";
+    function IniciaElementos() {
+        defineTituloDaPagina();
+        aplicaEstiloAosElementos();
+
+        $(document).on('click', btnFecharModal, function () {
+            fecharModal(this);
+        });
+    }
+
+    function aplicaEstiloAosElementos() {
+        $('.table').DataTable();
+
+        $(".select").select2({
+            placeholder: 'Selecione...',
+            theme: "classic",
+            language: {
+                noResults: function () {
+                    return "Informação não encontrada!";
+                }
+            }
+        })
+        $(".select-one").select2({
+            theme: "classic",
+            tags: true
+        })
+    }
+
+
+
+    function defineTituloDaPagina() {
+        let elTituloDaPagina = $(tituloDaPagina);
+        let elTituloDaPaginaLayout = document.getElementById(tituloDaPaginaLayout)
+
+        elTituloDaPaginaLayout.innerHTML = elTituloDaPagina.val();
+    }
+
+    function fecharModal(btnClicado) {
+        let modal = $('.modal');
+
+        for (let index = 0; index < modal.length; index++) {
+            let elModal = modal[index];
+            if (elModal.contains(btnClicado)) {
+                $('#' + elModal.id).modal('hide');
+                break;
             }
         }
-    })
-    $(".select-one").select2({
-        theme: "classic",
-        tags: true
-    })
-})
+    }
 
-function IniciaElementos() {
-    let elTituloDaPagina = $(tituloDaPagina);
-    let elTituloDaPaginaLayout = $(tituloDaPaginaLayout);
 
-    elTituloDaPaginaLayout[0].innerHTML =  elTituloDaPagina[0].value;
-}
+    // $(document).ready(function () {
+    //     $('.table').DataTable();
+
+    //     $(".select").select2({
+    //         placeholder: 'Selecione...',
+    //         theme: "classic",
+    //         language: {
+    //             noResults: function () {
+    //                 return "Informação não encontrada!";
+    //             }
+    //         }
+    //     })
+    //     $(".select-one").select2({
+    //         theme: "classic",
+    //         tags: true
+    //     })
+    // })
+})();
